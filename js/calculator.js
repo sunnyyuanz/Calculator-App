af1d4535bb05d6424db2723960e7645d74cc86f1
sunnyyuanz <84478918+sunnyyuanz@users.noreply.github.com>
2023-01-06 16:30:14 -0500

    OOP

--- js/calculator.js
+++ js/calculator.js
@@ -1,50 +1,39 @@
 const calButtons = document.querySelectorAll('.calc-button');
 let screen = document.querySelector('.screen');
-let temp = '0';
+let screenValue = '0';
 let result = 0;
 let previousOperation = null;
 
-// function buttonClicked(value) {
-//   if (isNaN(value)) {
-//     //this is a symbol
-//     console.log(typeof temp + 'this is a symbol');
-//     handleSymbols();
-//   } else {
-//     //this is a symbol
-//     console.log(typeof temp + 'this is a number');
-//     handleNums();
-//   }
-// }
 function buttonClicked(value) {
   if (isNaN(value)) {
     //this is a symbol
     handleSymbols(value);
   } else {
     //this is a symbol
-    handleNums(value);
+    printNumToScreen(value);
   }
-  screen.innerText = temp;
+  screen.innerText = screenValue;
 }
 
-function handleNums(numString) {
-  if (temp === '0') {
-    temp = numString;
+function printNumToScreen(numString) {
+  if (screenValue === '0') {
+    screenValue = numString;
   } else {
-    temp += numString;
+    screenValue += numString;
   }
 }
 
 function handleSymbols(symbol) {
   switch (symbol) {
     case 'C':
-      temp = '0';
+      screenValue = '0';
       result = 0;
       break;
     case '←':
-      if (temp.length === 1) {
-        temp = '0';
+      if (screenValue.length === 1) {
+        screenValue = '0';
       } else {
-        temp = temp.substring(0, temp.length - 1);
+        screenValue = screenValue.substring(0, screenValue.length - 1);
       }
       break;
     case '÷':
@@ -57,46 +46,41 @@ function handleSymbols(symbol) {
       if (previousOperation === null) {
         result;
       }
-      keepMathGoing(parseInt(temp));
+      handleMath(symbol);
       previousOperation = null;
-      temp = result;
+      screenValue = result;
       result = 0;
       break;
   }
 }
 
 function handleMath(symbol) {
-  if (temp === '0') {
+  if (screenValue === '0') {
     return;
   }
-  const tempNum = parseInt(temp); //or do +temp
+  var tempNum = parseInt(screenValue); //or do +temp
   if (result === 0) {
     result = tempNum;
   } else {
-    keepMathGoing(tempNum);
-    console.log(result);
+    switch (previousOperation) {
+      case '+':
+        result += tempNum;
+        break;
+      case '×':
+        result *= tempNum;
+        break;
+      case '−':
+        result -= tempNum;
+        break;
+      case '÷':
+        result /= tempNum;
+        break;
+    }
   }
 
   previousOperation = symbol; //keep the previous operation in track
 
-  temp = '0';
-}
-
-function keepMathGoing(tempNum) {
-  switch (previousOperation) {
-    case '+':
-      result += tempNum;
-      break;
-    case '×':
-      result *= tempNum;
-      break;
-    case '−':
-      result -= tempNum;
-      break;
-    case '÷':
-      result /= tempNum;
-      break;
-  }
+  screenValue = '0';
 }
 
 function init() {
